8cf914ccffc7d89ddb58119758080101d11f5ead
Ruslan Guskov <r.guskov@onpeak.ru>
2023-03-05 15:19:17 +0300

    После подключения БД, модуля авторизации и vue

--- www/routes/web.php
+++ www/routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/test/', function () {
+    return view('test');
 });
 
-Route::get('/catalog/', function () {
-    return view('catalog');
-});
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
